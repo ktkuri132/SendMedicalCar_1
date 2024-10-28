06df5a1cbcc1e23d8b55dbd6f7181761dc036915
m <3457532883@qq.com>
2024-10-28 22:49:44 +0800

    wifi

--- SMC/WIFI/wifi.c
+++ SMC/WIFI/wifi.c
@@ -1,2 +1,8 @@
-#include <pico/stdlib.h>
-#include <pico/types.h>
+#include <pico/stdio.h>
+#include <pico/cyw43_arch.h>
+
+void wifi_scan(void)
+{
+    cyw43_t *cyw43 = cyw43_get();
+    cyw43_scan(cyw43);
+}
